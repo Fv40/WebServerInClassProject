55c203ec757b3f87a26f719f85ccae63b344f27e
Fv40 <deckerd70@yahoo.com>
2025-03-25 20:52:16 -0400

    Added product controller and endpoint test file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const PORT = 8000;
 
 const express = require('express');
+const productsController = require('./controllers/products')
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
+app
+   .get('/', (req, res) => {
+     res.send('Hello New Paltz, NY!!!')
+   })
+   .use('/products', productsController)
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
 });
