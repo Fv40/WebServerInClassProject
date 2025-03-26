a10b0828035a132eb334c91e5dce0b3d9a24d90b
Fv40 <deckerd70@yahoo.com>
2025-03-25 20:43:14 -0400

    fixed product model, and removed products from home page

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,54 +1,19 @@
 <script setup lang="ts">
-import { getAll, type Product } from '@/models/products';
-
-const products = getAll()
-
-function addToCart(product: Product) {
-    console.log('Add to cart', product)
-}
 </script>
 
 <template>
-    <div>
-        <h1 class="title">Products</h1>
-        <div class="shelf">
-            <div class="product" v-for="p in products.items" :key="p.id">
-                <div class="product-image">
-                    <img :src="p.thumbnail" alt="product image" />
-                </div>
-                <div class="product-info">
-                    <h2>{{ p.title }}</h2>
-                    <p>{{ p.description }}</p>
-                    <span class="price">${{ p.price }}</span>
-                    <button class="button is-success" @click="addToCart(p)">Add to cart</button>
-                </div>
-            </div>
-        </div>
-    </div>
+  <main>
+     <h1 class="title">Anonymous Feedback Form</h1>
+     <h2 class="subtitle">Enter your feedback here...</h2>
+     <form action="#" class="form" method="post">
+       <textarea
+            class="textarea"
+            placeholder="Tell me about your favorite class">
+       </textarea>
+       <button class="button is-success mt-2" type="submit">Submit</button>
+     </form>
+   </main>
 </template>
 
 <style scoped>
-.shelf {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-}
-
-.shelf .product {
-    width: 30%;
-    margin: 1em;
-    padding: 1em;
-    border: 1px solid #ccc;
-    border-radius: 1em;
-}
-
-.price {
-    color: crimson;
-    font-weight: bold;
-    font-size: 1.5em;
-}
-
-.button.is-success {
-    float: right;
-}
 </style>
