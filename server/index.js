85649efd40d2442ff7babc6d9f25e6f1ed18cd29
Fv40 <deckerd70@yahoo.com>
2025-03-25 20:56:05 -0400

    formatting change

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const PORT = 8000;
 
-const express = require('express');
-const productsController = require('./controllers/products')
+const express = require("express");
+const productsController = require("./controllers/products");
 
 const app = express();
 
 app
-   .get('/', (req, res) => {
-     res.send('Hello New Paltz, NY!!!')
-   })
-   .use('/products', productsController)
-app.listen(PORT, () => {
+  .get("/", (req, res) => {
+    res.send("Hello New Paltz, NY!!!");
+  })
+  .use("/products", productsController)
+  .listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
-});
+  });
