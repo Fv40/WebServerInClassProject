235676cfcce199c97198937ba1c073fc508281d1
Fv40 <deckerd70@yahoo.com>
2025-04-10 19:00:36 -0400

    string assingment bug fix

--- client/src/pages/products/[id].vue
+++ client/src/pages/products/[id].vue
@@ -3,7 +3,7 @@ import { getOne } from '@/models/products'
 import { useRoute } from 'vue-router'
 
 const route = useRoute('/products/[id]')
-const product = getOne(route.params.id)
+const product = getOne(route.params.id as string)
 </script>
 
 <template>
